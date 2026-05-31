3ba13f90ba21f93a2e4d75350e937034f31942d2
Goutham Kumar <goutham4kumar24@gmail.com>
2026-05-31 16:47:41 +0530

    cicd change --no impact to reviwere

--- terraform/modules/confluent/versions.tf
+++ terraform/modules/confluent/versions.tf
@@ -1,7 +1,6 @@
 # Required to prevent Terraform from defaulting to "hashicorp/confluent",
 # which does not exist in the registry.
 terraform {
-terraform {
   required_providers {
     confluent = {
       source = "confluentinc/confluent"
